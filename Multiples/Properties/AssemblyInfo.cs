057f6dfa9e5fd9283af8460a34118999f34267b0
TITANSOFT\lizzy.yang71 <lizzy.yang@titansoft.com.sg>
2018-05-29 10:33:59 +0800

    finished Multiples of 3 or 5

---
+++ Multiples/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Multiples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Multiples")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98e33941-f02e-4ce3-81eb-ccf158e7a87b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Multiples/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Multiples
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Test()
+		{
+			Assert.AreEqual(23, Kata.Solution(10));
+		}
+	}
+}
